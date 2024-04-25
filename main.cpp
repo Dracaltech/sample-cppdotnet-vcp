ccf8d3fd60f6ff9b4494b785aeb80f1ea39b9fce
Mathieu Duchesneau <mathieu@doubledash.io>
2024-04-25 10:01:33 +0100

    cleanup & add comments in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,16 @@
-#using <System.dll>
-#using "System.Linq.dll"
-
-using namespace System;
-using namespace System::Collections::Generic;
-using namespace System::IO::Ports;
-using namespace System::Linq;
-using namespace System::Runtime::InteropServices;
-using namespace System::Threading;
-
 #include <msclr/gcroot.h>
+#using <System.dll>
+
 using namespace msclr;
+using namespace System;
+using namespace System::IO::Ports;
+using namespace System::Threading;
 
 const auto PATH = "COM3";
 const int BAUDRATE = 9600;
 const int INTERVAL = 1000;
 
+// allow ref type static storage w/ gcroot()
 gcroot<array<String^>^> info_line;
 int padlen = 0;
 
@@ -22,69 +18,22 @@ int GetMaxLength(array<String^>^ split)
 {
     int maxLength = 0;
     for each (auto str in split) {
-        maxLength = str->Length > maxLength ? str->Length : maxLength;
+        maxLength = std::max(str->Length, maxLength);
     }
-
     return maxLength;
-}
-
-array<String^>^ GetSensors(array<String^>^ info_line)
-{
-    List<String^>^ sensors = gcnew List<String^>();
-
-    for (int i = 4; i < info_line->Length; i += 2)
-    {
-        sensors->Add(info_line[i]);
-    }
-
-    return sensors->ToArray();
-}
-
-array<String^>^ GetValues(array<String^>^ split)
-{
-    List<String^>^ values = gcnew List<String^>();
-
-    for (int i = 0; i < split->Length; i += 2)
-    {
-        values->Add(split[i]);
-    }
-
-    return values->ToArray();
-}
-
-array<String^>^ GetUnits(array<String^>^ split)
-{
-    List<String^>^ units = gcnew List<String^>();
-
-    for (int i = 1; i < split->Length; i += 2)
-    {
-        units->Add(split[i]);
-    }
-
-    return units->ToArray();
-}
-
-String^ GetCurrentTime()
-{
-    DateTime currentTime = DateTime::Now;
-    return String::Format("{0} {1}", currentTime.ToShortDateString(), currentTime.ToShortTimeString());
 }
 
 void handleReceivedData(SerialPort^ port)
 {
     String^ data = port->ReadLine();
-    //List<String^>^ split = Enumerable::ToList(safe_cast<IEnumerable<String^>^>(data->Split(',')));
     array<String^>^ split = data->Split(',');
 
     if (split[0] == "I")
     {
         if (split[1] == "Product ID")
         {
-            //TODO
             info_line = split;
-            //TODO
             padlen = GetMaxLength(split);
-            //TODO
             Console::WriteLine(String::Join(",", info_line));
         }
         else
@@ -105,23 +54,19 @@ void handleReceivedData(SerialPort^ port)
     array<Double^>^ values = gcnew array<Double^>(sensors->Length);
     array<String^>^ units = gcnew array<String^>(sensors->Length);
 
+    // map value positions into dedicated arrays
     for (int i = 0; i < info_line->Length - 5; i += 2) {
         sensors[i / 2] = info_line[i + 4]->Trim();
         values[i / 2] = Double::Parse(split[i + 4]);
         units[i / 2] = split[i + 5]->Trim();
     }
 
-    /*String^ device = info_line[4] + " " + info_line[5];
-    array<String^>^ sensors = GetSensors(info_line);
-    array<String^>^ values = GetValues(split);
-    array<String^>^ units = GetUnits(split);*/
-
-    Console::WriteLine(GetCurrentTime() + " " + device);
+    // display results
+    Console::WriteLine(String::Format("\n{0} {1} {2}", DateTime::Now.ToShortDateString(), DateTime::Now.ToShortTimeString(), device));
     for (int i = 0; i < units->Length; i++)
     {
-        Console::WriteLine(String::Format("{0} {1} {2}", sensors[i]->PadLeft(padlen + 2, ' '), values[i], units[i]));
+        Console::WriteLine(String::Format("{0} {1} {2}", sensors[i]->PadRight(padlen + 2, ' '), values[i], units[i]));
     }
-    Console::WriteLine("\n");
 }
 
 void Console_CancelKeyPress(Object^ sender, ConsoleCancelEventArgs^ e)
@@ -147,6 +92,8 @@ int main(array<String^>^ args)
         port->WriteLine("INFO");
         Thread::Sleep(100);
 
+        // NOTE: while the standard approach would be to use Event handler `port.DataReceived`, it has
+        // proven unable to receive non-input driven readout data thus far.
         while (port->IsOpen)
         {
             handleReceivedData(port);
